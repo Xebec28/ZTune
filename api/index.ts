2a5ff4b8070f500ff835c61b1fe1c4097d33b17a
Xebec28 <agentmathew19@gmail.com>
2026-08-18 20:12:50 +0530

    Fix Vercel server import

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { createApiApp } from '../src/server/app';
+import { createApiApp } from '../src/server/app.js';
 
 // Vercel Serverless Function entry point
 const app = createApiApp();
